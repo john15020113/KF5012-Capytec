2c899507366221ed65fc1905a139ffe1dec76891
john15020113 <35560338+john15020113@users.noreply.github.com>
2018-05-08 10:23:56 +0100

    Delete AdminProcess.java

--- AdminProcess.java
+++
@@ -1,9 +0,0 @@
-package capytec;
-
-public class AdminProcess {
-	private DatabaseConn dbConn = new DatabaseConn();
-	
-	public boolean createAccount(String username, String password, boolean admin) {
-		
-	}
-}
